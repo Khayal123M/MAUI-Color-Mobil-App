325635a1ccd06b3e9289d451c1afa3e5ffb03477
Khayal123M <132755333+Khayal123M@users.noreply.github.com>
2023-05-11 17:05:13 +0300

    Add files via upload
    You can create different colors by mixing the intensity levels of Red, Green, and Blue using three Sliders. Additionally, you can use a Random button to randomly mix these three colors. The color codes of the colors are then displayed in a label.
    
    .NET MAUI (Multi-platform App UI) is an open-source framework for building cross-platform mobile and desktop applications. It is the evolution of Xamarin.Forms and is designed to create native user interfaces that can run on multiple platforms, including iOS, Android, Windows, macOS, and more.
    
    .NET MAUI allows developers to write a single codebase using C# and XAML, and deploy their applications to various platforms without sacrificing native performance and user experience. It provides a rich set of controls, layouts, and services that can be used to create responsive and visually appealing applications.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System.Diagnostics;
+
+namespace Net_MAUI_Color_Mobil_App
+{
+    public partial class MainPage : ContentPage
+    {
+       
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            var red = sldRed.Value;  
+            var green = sldGreen.Value;
+            var blue = sldBlue.Value;
+
+            Color color = Color.FromRgb(red, green, blue);        
+
+            SetColor(color);  
+        }
+
+        private void SetColor(Color color)
+        {
+            Debug.WriteLine(color.ToString());
+            btnRandom.Background = color; 
+            Container.BackgroundColor = color; 
+            lblHex1.Text = color.ToHex(); 
+        }
+
+        private void btn_Random_Clicked(object sender, EventArgs e)  
+        {
+            var random = new Random(); 
+            var color = Color.FromRgb(  
+                random.Next(0, 256),
+                random.Next(0, 256),
+                random.Next(0, 256));
+
+            SetColor(color);     
+
+            sldRed.Value = color.Red;      
+            sldGreen.Value = color.Green;
+            sldBlue.Value = color.Blue;
+        }
+
+
+
+
+
+
+
+    }
+}
